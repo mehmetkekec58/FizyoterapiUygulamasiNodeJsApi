e8175b06d01d1687e96abf13e8f3f46eab4e3180
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-02-15 16:55:50 +0300

    15.02.2022

--- databases/databaseConfig/postgreSql.js
+++ databases/databaseConfig/postgreSql.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'Northwind',
+  database: 'test',
   password: '12345',
   port: 5432,
 })
